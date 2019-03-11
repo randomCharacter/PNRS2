97e9e8bd221b148ac729422ef3fe5e5a7e08c626
Mario Perić <pericmario96@gmail.com>
2019-03-11 16:23:00 +0100

    vezba01

---
+++ vezba01/zadatak2/semafor.vhd
@@ -0,0 +1,149 @@
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+
+entity semafor is
+    Port ( clk : in  STD_LOGIC;
+           reset_n : in  STD_LOGIC;
+           Sel_in : in  STD_LOGIC_VECTOR (1 downto 0);
+           RGB_A : out  STD_LOGIC_VECTOR (2 downto 0);
+           RGB_B : out  STD_LOGIC_VECTOR (2 downto 0));
+end semafor;
+
+architecture Behavioral of semafor is
+signal count_s : STD_LOGIC_VECTOR(15 downto 0);
+type states_t is (IDLE, GREEN, YELLOW1, RED, YELLOW2);
+signal state, state_next : states_t;
+begin
+
+    process (clk, reset_n) begin
+        if reset_n = '0' then
+            count_s <= (others => '0');
+        elsif rising_edge(clk) then
+            if Sel_in = "00" then
+                case state is 
+                    when IDLE =>
+                        state_next <= GREEN;
+                    when GREEN =>
+                        if (count_s >= 30) then
+                            state_next <= YELLOW1;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= GREEN;
+                            count_s <= count_s + 1;
+                        end if;
+                    when YELLOW1 =>
+                        if (count_s >= 7) then
+                            state_next <= RED;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW1;
+                            count_s <= count_s + 1;
+                        end if;
+                    when RED =>
+                        if (count_s >= 30) then
+                            state_next <= YELLOW2;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= RED;
+                            count_s <= count_s + 1;
+                        end if;
+                    when others =>
+                        if (count_s >= 7) then
+                            state_next <= GREEN;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW2;
+                            count_s <= count_s + 1;
+                        end if;
+                end case;
+            elsif Sel_in = "01" then
+                case state is 
+                    when IDLE =>
+                        state_next <= GREEN;
+                    when GREEN =>
+                        if (count_s >= 40) then
+                            state_next <= YELLOW1;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= GREEN;
+                            count_s <= count_s + 1;
+                        end if;
+                    when YELLOW1 =>
+                        if (count_s >= 7) then
+                            state_next <= RED;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW1;
+                            count_s <= count_s + 1;
+                        end if;
+                    when RED =>
+                        if (count_s >= 20) then
+                            state_next <= YELLOW2;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= RED;
+                            count_s <= count_s + 1;
+                        end if;
+                    when others =>
+                        if (count_s >= 7) then
+                            state_next <= GREEN;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW2;
+                            count_s <= count_s + 1;
+                        end if;
+                end case;
+            else
+                case state is 
+                    when IDLE =>
+                        state_next <= GREEN;
+                    when GREEN =>
+                        if (count_s >= 20) then
+                            state_next <= YELLOW1;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= GREEN;
+                            count_s <= count_s + 1;
+                        end if;
+                    when YELLOW1 =>
+                        if (count_s >= 7) then
+                            state_next <= RED;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW1;
+                            count_s <= count_s + 1;
+                        end if;
+                    when RED =>
+                        if (count_s >= 40) then
+                            state_next <= YELLOW2;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= RED;
+                            count_s <= count_s + 1;
+                        end if;
+                    when others =>
+                        if (count_s >= 7) then
+                            state_next <= GREEN;
+                            count_s <= (others => '0');
+                        else
+                            state_next <= YELLOW2;
+                            count_s <= count_s + 1;
+                        end if;
+                end case;
+            end if;
+        end if;
+    end process;
+
+    state <= state_next;
+
+    RGB_A <= "001" when state = GREEN else
+             "010" when state = YELLOW1 or state = YELLOW2 else
+             "100";
+    RGB_B <= "100" when state = GREEN else
+             "010" when state = YELLOW1 or state = YELLOW2 else
+             "001";
+
+end Behavioral;
+
---
+++ vezba01/zadatak2/semafor_tb.vhd
@@ -0,0 +1,73 @@
+
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ 
+ENTITY semafor_tb IS
+END semafor_tb;
+ 
+ARCHITECTURE behavior OF semafor_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT semafor
+    PORT(
+         clk : IN  std_logic;
+         reset_n : IN  std_logic;
+         Sel_in : IN  std_logic_vector(1 downto 0);
+         RGB_A : OUT  std_logic_vector(2 downto 0);
+         RGB_B : OUT  std_logic_vector(2 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset_n : std_logic := '0';
+   signal Sel_in : std_logic_vector(1 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal RGB_A : std_logic_vector(2 downto 0);
+   signal RGB_B : std_logic_vector(2 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: semafor PORT MAP (
+          clk => clk,
+          reset_n => reset_n,
+          Sel_in => Sel_in,
+          RGB_A => RGB_A,
+          RGB_B => RGB_B
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      reset_n <= '0';
+      wait for 100 ns;
+      reset_n <= '1';	
+
+      wait for clk_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
